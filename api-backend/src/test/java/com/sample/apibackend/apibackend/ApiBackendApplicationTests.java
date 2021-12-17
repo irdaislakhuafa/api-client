868f11a47e007ea60ef82610ddf5bd2ddd7ff7b6
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2021-12-17 13:01:02 +0700

    First commit

---
+++ api-backend/src/test/java/com/sample/apibackend/apibackend/ApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.apibackend.apibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
