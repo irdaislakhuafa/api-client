92820ef0b103fb0925c9ed7d9a4a016fb4702619
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2021-12-17 14:40:15 +0700

    create bean RestTemplate

--- src/main/java/com/sample/apiclient/ApiClientApplication.java
+++ src/main/java/com/sample/apiclient/ApiClientApplication.java
@@ -1,4 +1,4 @@
-package com.sample.apiclient.apiclient;
+package com.sample.apiclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
